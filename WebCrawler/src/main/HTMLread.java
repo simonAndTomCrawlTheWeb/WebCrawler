3a6cd7c8580b7d6b13dfc842fcd04f217adfc4c7
tohaga01 <t.r.ohagan@hotmail.co.uk>
2013-03-07 20:29:09 +0000

    Added interface detail.

--- WebCrawler/src/main/HTMLread.java
+++ WebCrawler/src/main/HTMLread.java
@@ -3,10 +3,14 @@
  */
 package main;
 
+import java.io.InputStream;
+
 /**
- * @author tohaga01
+ * @author tomAndSimon
  *
  */
 public interface HTMLread {
-
+	boolean readUntil(InputStream stream,char ch1,char ch2);
+	char skipSpace(InputStream stream,char ch);
+	String readString(InputStream stream,char ch1,char ch2);
 }
