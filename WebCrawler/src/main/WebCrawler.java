db2c56e1b7627e218af9e649af9d4881e3d3334e
Thomas O'Hagan <t.r.ohagan@hotmail--co--uk>
2013-03-17 15:06:08 +0000

    Added some comments for clarity.

--- WebCrawler/src/main/WebCrawler.java
+++ WebCrawler/src/main/WebCrawler.java
@@ -31,6 +31,10 @@ public abstract class WebCrawler {
 		this.maxLinks = maxLinks;
 	}
 	
+	/*
+	 * Would like to this recursively; but, started out
+	 * by doing it iteratively- refactor later.
+	 */
 	public final void crawl(String url,String dbFile) {
 		
 		File file = new File(dbFile); //What if dbFile is null? Handle NullPointerException?
@@ -40,19 +44,18 @@ public abstract class WebCrawler {
 			db = new Database();
 		}
 		
-		int numberOfLinksFound = 1;
-		int currentPriority = 0;
-		int index = 0;
-		db.addLink(currentPriority,url);
+		int numberOfLinksFound = 1; //Including url given to method as one of the links
+		int currentPriority = 0; //The url parameter is the only link with priority 0
+		int index = 0; //The index of the current link in linksToCrawl.get(currentPriority)
+		db.addLink(currentPriority,url); //Add url to links database
 
-		while (currentPriority <= maxDepth && numberOfLinksFound < maxLinks) {
+		while (currentPriority <= maxDepth && numberOfLinksFound < maxLinks) { //Stops user-settings on depth and link number being exceeded
 			try {
-				Document htmlDoc = Jsoup.connect(url).get();
-				Elements allATags = htmlDoc.select("a[href]");
-				System.out.println("Number of A tags: " + allATags.size());
-				int i = 0;
-				while ((i < allATags.size()) && (numberOfLinksFound < maxLinks)) {
-					db.addLink(currentPriority+1,allATags.get(i).attr("abs:href").toString());
+				Document htmlDoc = Jsoup.connect(url).get(); //Parse page (see Jsoup API)
+				Elements allATags = htmlDoc.select("a[href]"); //Find all 'a-tags' with an 'href'-attribute
+				int i = 0; //The index of allATags
+				while ((i < allATags.size()) && (numberOfLinksFound < maxLinks)) { //Stops maxLinks being exceeded and IndexOutOfBoundsExceptions on allATags
+					db.addLink(currentPriority+1,allATags.get(i).attr("abs:href").toString()); //Adds absolute address of links found (with relevant priority)
 					numberOfLinksFound++;
 					i++;
 				}
@@ -61,22 +64,26 @@ public abstract class WebCrawler {
 				ex.printStackTrace();
 			}
 			
-			//'Future programmers' search functionality
+			/*
+			 * 'Future programmers' search functionality
+			 */
 			boolean result = search(url);
 			if (result) {
 				db.addResult(url);
 			}
 			
 			index++;
-			if (index == db.getLinksToCrawl().get(currentPriority).size()) {
+			if (index == db.getLinksToCrawl().get(currentPriority).size()) { //Checks if we have crawled all the links in the current priority group
 				currentPriority++;
 				index = 0;
-				if (db.getLinksToCrawl().get(currentPriority) != null) {
-					url = db.getLinksToCrawl().get(currentPriority).get(index);
+				if (db.getLinksToCrawl().get(currentPriority) != null) { //Checks if there are any links of a higher priority that need to be crawled
+					url = db.getLinksToCrawl().get(currentPriority).get(index); //Update url-"to crawl" to first element of the next higher priority group
 				} else {
-					numberOfLinksFound = maxLinks;
+					//There are no higher priority links to crawl
+					return;
 				}
 			} else {
+				//Update url-"to crawl" to be the next element along in the current priority group
 				url = db.getLinksToCrawl().get(currentPriority).get(index);
 			}
 		}
